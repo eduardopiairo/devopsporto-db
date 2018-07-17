e3687b1b01db7a75f4dfdce873b8428dcb6d7276
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-18 00:26:44 +0100

    drop test database

--- tSQLt/sql/drop-test-db.sql
+++ tSQLt/sql/drop-test-db.sql
@@ -1,3 +1,6 @@
 -- drop database
-ALTER DATABASE [DevOpsPorto-Db-Test] SET single_user WITH ROLLBACK IMMEDIATE
-DROP DATABASE [DevOpsPorto-Db-Test]
+IF EXISTS (SELECT name FROM master.sys.databases WHERE name = N'DevOpsPorto-Db-Test')
+BEGIN
+    ALTER DATABASE [DevOpsPorto-Db-Test] SET single_user WITH ROLLBACK IMMEDIATE
+    DROP DATABASE [DevOpsPorto-Db-Test]
+END
