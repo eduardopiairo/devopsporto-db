b1863d9f0a3561803965d570668abe7a6fe1912f
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-17 16:15:16 +0100

    database name test

--- tSQLt/sql/create-test-database.sql
+++ tSQLt/sql/create-test-database.sql
@@ -1,2 +1,2 @@
-IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = N'DevOpsPorto-Test')
-    CREATE DATABASE [DevOpsPorto-Test]
+IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = N'DevOpsPorto-Db-Test')
+    CREATE DATABASE [DevOpsPorto-Db-Test]
