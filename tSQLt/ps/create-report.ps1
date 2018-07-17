4bbc8f3a9f7be4e0f1372ed40985aff267d19b0e
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-17 23:17:44 +0100

    create test report

--- tSQLt/ps/create-report.ps1
+++ tSQLt/ps/create-report.ps1
@@ -1,10 +1,7 @@
 param(
         [Parameter(Mandatory=$true)][string]$serverName,
-        [Parameter(Mandatory=$true)][string]$databaseName,
-        [Parameter(Mandatory=$true)][string]$user,
-        [Parameter(Mandatory=$true)][string]$pass
-
+        [Parameter(Mandatory=$true)][string]$databaseName
     )
 
 
-SQLCMD.EXE -E -b -S "EDPIAIRO\EDP17" -d "DevOpsPorto-db-test" -i ".\tSQLt\sql\get-test-results.sql" -o "..\TestResults\TestResults.xml"
+SQLCMD.EXE -E -b -S "$serverName" -d "$databaseName" -i ".\tSQLt\sql\get-test-results.sql" -o "..\TestResults\TestResults.xml"
