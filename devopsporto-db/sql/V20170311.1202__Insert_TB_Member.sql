64d28094637f4b1e18e3ea0787a3ada4935d53a1
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-17 17:16:43 +0100

    migrations fix

--- devopsporto-db/sql/V20170311.1202__Insert_TB_Member.sql
+++ devopsporto-db/sql/V20170311.1202__Insert_TB_Member.sql
@@ -1,2 +1,2 @@
 INSERT INTO [dbo].[Member] ([MemberUserName],[MemberFirstName],[MemberLastName]) VALUES ('ed.piairo','Eduardo','Piairo')
-INSERT INTO [dbo].[Member] ([MemberUserName],[MemberFirstName],[MemberLastName]) VALUES ('garlic.mike''Miguel','Alho')
+INSERT INTO [dbo].[Member] ([MemberUserName],[MemberFirstName],[MemberLastName]) VALUES ('garlic.mike','Miguel','Alho')
