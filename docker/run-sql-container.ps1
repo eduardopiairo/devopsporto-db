875a15764f6ed7a4b08c9af2ac7d5ee92abded44
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-18 16:30:31 +0100

    run container

--- docker/run-sql-container.ps1
+++ docker/run-sql-container.ps1
@@ -1,7 +1,6 @@
 param(
-        [Parameter(Mandatory=$true)][int]$containerPort,
         [Parameter(Mandatory=$true)][string]$containerName,
-        [Parameter(Mandatory=$true)][string]$containerPassword
+        [Parameter(Mandatory=$true)][string]$containerPass
     )
 
-docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$containerPassword" -p $containerPort:1433 --name $containerName -d microsoft/mssql-server-linux:2017-latest
+docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$containerPass" -p 1533:1433 --name $containerName -d microsoft/mssql-server-linux:2017-latest
