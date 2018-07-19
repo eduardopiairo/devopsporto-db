f2a309f41431d0c28f37e115747565f18de1b8f0
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-19 14:21:16 +0100

    pester test for transparent data encryption

--- pester/Get-AzureDb-TransparentDataEncryption.Tests.ps1
+++ pester/Get-AzureDb-TransparentDataEncryption.Tests.ps1
@@ -3,35 +3,10 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Get-AzureDb-TransparentDataEncryption" {
-    Context "dcn-live-sqlserver"{
-        It "TransparentDataEncryption for dcn-live-basecone-db (dcn-live-sqlserver) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver" "dcn-live-basecone-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-document-db (dcn-live-sqlserver) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver" "dcn-live-document-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-folder-db (dcn-live-sqlserver) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver" "dcn-live-folder-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-identity-db (dcn-live-sqlserver) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver" "dcn-live-identity-db" | Should -Be $true
+    Context "devopsporto-db-sqlserver"{
+        It "TransparentDataEncryption for DevOpsPorto-Db (devopsporto-db-sqlserver) should Enabled " {
+            Get-AzureRmSqlDatabaseTransparentDataEncryption "tugait2018" "devopsporto-db-sqlserver" "DevOpsPorto-Db" | Should -Be $true
         }
     }
-    
-    Context "dcn-live-sqlserver-north"{
-        It "TransparentDataEncryption for dcn-live-basecone-db (dcn-live-sqlserver-north) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver-north" "dcn-live-basecone-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-document-db (dcn-live-sqlserver-north) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver-north" "dcn-live-document-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-folder-db (dcn-live-sqlserver-north) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver-north" "dcn-live-folder-db" | Should -Be $true
-        }
-        It "TransparentDataEncryption for dcn-live-identity-db (dcn-live-sqlserver-north) should Enabled " {
-            Get-AzureRmSqlDatabaseTransparentDataEncryption "dcn-live" "dcn-live-sqlserver-north" "dcn-live-identity-db" | Should -Be $true
-        }
-    }
-    
 }
 
