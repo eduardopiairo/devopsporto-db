35f4868773aaa64aa1de0fc039b2bac96ed4c86a
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-19 14:12:38 +0100

    pester tests for azure

--- pester/Get-AzureDb-GeoReplication.Tests.ps1
+++ pester/Get-AzureDb-GeoReplication.Tests.ps1
@@ -3,20 +3,7 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "AzureDb-GeoReplication" {
-    
-    It "dcn-live-basecone-db should have geo replication enabled" {
-        Get-AzureDbGeoReplication "dcn-live-basecone-db" | Should -Be $true
-    }
-
-    It "dcn-live-document-db should have geo replication enabled" {
-        Get-AzureDbGeoReplication "dcn-live-document-db" | Should -Be $true
-    }
-
-    It "dcn-live-folder-db should have geo replication enabled" {
-        Get-AzureDbGeoReplication "dcn-live-folder-db" | Should -Be $true
-    }
-
-    It "dcn-live-identity-db should have geo replication enabled" {
-        Get-AzureDbGeoReplication "dcn-live-identity-db" | Should -Be $true
+    It "DevOpsPOrto-Db should have geo replication enabled" {
+        Get-AzureDbGeoReplication "DevOpsPOrto-Db" | Should -Be $true
     }
 }
