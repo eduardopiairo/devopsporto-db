0ef54987e6101e5db3da8e39774443a80d333a5d
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-19 14:03:42 +0100

    pester tests for azure

---
+++ pester/Get-AzureDb-GeoReplication.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "AzureDb-GeoReplication" {
+    
+    It "dcn-live-basecone-db should have geo replication enabled" {
+        Get-AzureDbGeoReplication "dcn-live-basecone-db" | Should -Be $true
+    }
+
+    It "dcn-live-document-db should have geo replication enabled" {
+        Get-AzureDbGeoReplication "dcn-live-document-db" | Should -Be $true
+    }
+
+    It "dcn-live-folder-db should have geo replication enabled" {
+        Get-AzureDbGeoReplication "dcn-live-folder-db" | Should -Be $true
+    }
+
+    It "dcn-live-identity-db should have geo replication enabled" {
+        Get-AzureDbGeoReplication "dcn-live-identity-db" | Should -Be $true
+    }
+}
---
+++ pester/Get-AzureDb-GeoReplication.ps1
@@ -0,0 +1,24 @@
+﻿function Get-AzureDbGeoReplication {
+
+    param(
+        [Parameter(Mandatory=$true)][string]$databaseName
+    )
+
+    $rg = "dcn-live"
+
+    $sqlServer = "dcn-live-sqlserver"
+
+    $replicationInfo = Get-AzureRmSqlDatabaseReplicationLink -ResourceGroupName $rg -ServerName $sqlServer -DatabaseName $databaseName -PartnerResourceGroupName $rg
+
+    if($replicationInfo -eq $null)
+    {
+        return $false
+    }
+    else {
+
+        if($replicationInfo.PartnerLocation -eq "Germany Northeast")
+        {
+            return $true
+        }
+    }
+}
