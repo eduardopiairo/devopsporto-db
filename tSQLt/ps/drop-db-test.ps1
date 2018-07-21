5242ad55dc8ee930da5def96b0a38717d6c0623a
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-21 12:33:55 +0100

    test phase drop test database

--- tSQLt/ps/drop-db-test.ps1
+++ tSQLt/ps/drop-db-test.ps1
@@ -6,4 +6,6 @@ param(
     )
 
 #Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Username $user -Password $pass -InputFile ".\tSQLt\sql\drop-test-db.sql"
+
+Write-Host $$serverName
 sqlcmd -S "$serverName" -d "master" -U $user -P $pass -i ".\tSQLt\sql\drop-test-db.sql"
