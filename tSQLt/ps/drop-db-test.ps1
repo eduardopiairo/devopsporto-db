4be1e7cc1472b5f563714ae6810c616e3634cbab
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-07-21 14:46:32 +0100

    trying to fix drop test database

--- tSQLt/ps/drop-db-test.ps1
+++ tSQLt/ps/drop-db-test.ps1
@@ -1,14 +1,9 @@
 
-#param(
-#        [Parameter(Mandatory=$true)][string]$serverName,
-#        [Parameter(Mandatory=$true)][string]$user,
-#        [Parameter(Mandatory=$true)][string]$pass
-#
-#    )
+param(
+        [Parameter(Mandatory=$true)][string]$serverName,
+        [Parameter(Mandatory=$true)][string]$user,
+        [Parameter(Mandatory=$true)][string]$pass
+    )
 
 #Invoke-Sqlcmd -ServerInstance $serverName -Database "master" -Username $user -Password $pass -InputFile ".\tSQLt\sql\drop-test-db.sql"
-
-#Write-Host $serverName
-
-
-sqlcmd -S "(local)\SQL2017" -d "master" -U "sa" -P "Password12!" -i ".\tSQLt\sql\drop-test-db.sql"
+sqlcmd -S $serverName -d "master" -U $user -P $pass -i ".\tSQLt\sql\drop-test-db.sql"
