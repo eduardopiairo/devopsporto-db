4dabf5d72df0fd82b9584c8d1b63f3e07a3ec430
edpiairo-ux360 <eduardopiairo@gmail.com>
2019-02-24 18:10:54 +0000

    tests demo review

--- tSQLt/tests/testMember/test_member_table_exist.sql
+++ tSQLt/tests/testMember/test_member_table_exist.sql
@@ -2,6 +2,8 @@ CREATE PROCEDURE testMember.[test that table Member exist]
 AS
 BEGIN
 
+--TEST PURPOSE: test if the table Member exist on the database
+
 ------Assert
     EXEC tSQLt.AssertObjectExists 'Member';
 END;
