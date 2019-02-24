226ab65717d36799c1617a3d50a3b146b515d372
edpiairo-ux360 <eduardopiairo@gmail.com>
2019-02-24 18:39:42 +0000

    readme update with data model

--- devopsporto-db/sql/V20180126.2330__Alter_TB_Member.sql
+++ devopsporto-db/sql/V20180126.2330__Alter_TB_Member.sql
@@ -1,1 +1,8 @@
 ALTER TABLE Member ADD MemberRoleId INT;
+
+ALTER TABLE [dbo].[Member]  WITH CHECK ADD  CONSTRAINT [FK_Member_Role] FOREIGN KEY([MemberRoleId])
+REFERENCES [dbo].[Role] ([RoleId])
+GO
+
+ALTER TABLE [dbo].[Member] CHECK CONSTRAINT [FK_Member_Role]
+GO
