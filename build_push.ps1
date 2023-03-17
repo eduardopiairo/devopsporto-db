96f702bebe005c5b42eec1e61ac9f81099de658e
eduardopiairo <eduardopiairo@gmail.com>
2023-03-17 10:39:39 +0000

    build push

--- build_push.ps1
+++ build_push.ps1
@@ -10,6 +10,7 @@ $packFullPath = $packPath + $buildNumber + $packExtension
 
 Write-Output $packFullPath
 
-nuget push $packFullPath -Source http://localhost:8090/nuget/packages -ApiKey $apiKey 
+# nuget push $packFullPath -Source http://localhost:8090/nuget/packages -ApiKey $apiKey 
+nuget push $packFullPath -Source https://pipe.octopus.app/nuget/packages -ApiKey $apiKey 
 
 exit $LastExitCode
